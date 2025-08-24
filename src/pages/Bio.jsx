d75f9c0403607537babb5610687dc6406fa249fb
Sjoseph42 <sandovaljoseph42@gmail.com>
2025-08-24 12:38:05 -0700

    Update Bio.jsx
    minor errors

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -4,7 +4,7 @@ export function Bio() {
   return (
     <section>
       <h1>About Me</h1>
-      <p>Hello, I’m Joey Sandoval—a dedicated programmer and aspiring computer engineer with a strong foundation in software development and computer science. I graduated from Sonoma State University in 2023 with a Master of Arts in Computer Science and recently sharpened my full-stack skills through Oregon State University’s intensive Programming Boot Camp.
+      <p>Hello, I’m Joey Sandoval—a dedicated programmer and aspiring computer engineer with a strong foundation in software development and computer science. I graduated from Sonoma State University in 2023 with a Master of Arts and recently sharpened my full-stack skills through Oregon State University’s intensive Programming Boot Camp.
 
 Over the past few years, I’ve built and deployed applications using JavaScript (React, Node.js), Python (Flask, Django), and cloud services. I thrive on translating complex problems into clean, maintainable code and collaborating in agile teams to deliver real-world solutions.
 
